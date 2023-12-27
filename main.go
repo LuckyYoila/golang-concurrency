8791a692190a2324a3baa880dfe9a7164869b207
LuckyYoila <luckyyoila626@gmail.com>
2023-12-27 19:05:08 +0100

    formatted

--- main.go
+++ main.go
@@ -10,12 +10,12 @@ func main() {
 	start := time.Now() //500ms
 	task1 := task1()
 	responseChannel := make(chan any, 2) //initialize channel of size 2
-	
+
 	waitGroup := &sync.WaitGroup{} // wait group that will wait for all tasks to finish
 	waitGroup.Add(2)
 
 	go task2(task1, responseChannel, waitGroup) //task2 will depend on task1
-	go task3(task1, responseChannel,  waitGroup) //task3 will depend on task1
+	go task3(task1, responseChannel, waitGroup) //task3 will depend on task1
 
 	waitGroup.Wait() //wait for all tasks to finish
 
@@ -37,15 +37,15 @@ func task1() int {
 	time.Sleep(500 * time.Millisecond) //lasts 500ms
 	return 1
 }
-func task2( dependency int, resChan chan any, wg *sync.WaitGroup) {
+func task2(dependency int, resChan chan any, wg *sync.WaitGroup) {
 	time.Sleep(550 * time.Millisecond) // lasts 550ms
-	
+
 	fmt.Println("T2 recieved dependency: ", dependency)
 
 	resChan <- 1 //pass a return value to channel
-	wg.Done() //inform wait group that task2 is done
+	wg.Done()    //inform wait group that task2 is done
 }
-func task3( dependency int, resChan chan any, wg *sync.WaitGroup) {
+func task3(dependency int, resChan chan any, wg *sync.WaitGroup) {
 	time.Sleep(600 * time.Millisecond) // lasts 600ms
 
 	fmt.Println("T3 recieved dependency: ", dependency)
